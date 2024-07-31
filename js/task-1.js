072cd89915406c61ba72dde34c30eb54254fb953
irina <irinarein49@gmail.com>
2024-07-31 18:20:49 +0300

    add fix-js

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 `use strict`;
 
 function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity + pricePerDroid;
-    console.log (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-    return totalPrice;
+    const totalPrice = quantity * pricePerDroid;
+    return  `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
